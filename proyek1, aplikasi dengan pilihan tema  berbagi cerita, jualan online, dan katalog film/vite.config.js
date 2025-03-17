b8e51672b7d4ce1f48dce0d1d78102f3cd7f64b0
toopique <sossmeed@gmail.com>
2025-03-17 12:01:48 +0700

    update

--- proyek1, aplikasi dengan pilihan tema  berbagi cerita, jualan online, dan katalog film/vite.config.js
+++ proyek1, aplikasi dengan pilihan tema  berbagi cerita, jualan online, dan katalog film/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  root: resolve(__dirname, 'src'),
+  publicDir: resolve(__dirname, 'src', 'public'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
